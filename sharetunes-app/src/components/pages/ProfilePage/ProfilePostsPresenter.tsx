d65bc72e30880958634be34cee84aaaca85ad3e2
Rasmus Rudling <rasmusrudling@gmail.com>
2021-05-11 15:18:03 +0200

    feat: show correct posts on profile page and home page

--- sharetunes-app/src/components/pages/ProfilePage/ProfilePostsPresenter.tsx
+++ sharetunes-app/src/components/pages/ProfilePage/ProfilePostsPresenter.tsx
@@ -5,15 +5,31 @@ import {
     useLoggedInUserUpdate,
 } from '../../../contexts/LoggedInUserContext';
 import { useDatabase } from '../../../contexts/DatabaseContext';
+import { Post } from '../../../utility/types';
 
 interface Props {}
 
 const ProfilePostsPresenter: React.FC<Props> = () => {
-    const user = useLoggedInUser();
+    const loggedInUser = useLoggedInUser();
     const setUser = useLoggedInUserUpdate();
     const { posts } = useDatabase();
 
-    return <ProfilePostsView posts={posts} />;
+    let postsToShow: Array<Post> = [];
+
+    if (loggedInUser) {
+        postsToShow = posts.filter(post => post.emailOfPublisher === loggedInUser.email);
+        postsToShow = postsToShow.sort((postA, postB) => {
+            if (postA.date < postB.date) {
+                return 1;
+            } else if (postA.date > postB.date) {
+                return -1;
+            } else {
+                return 0;
+            }
+        })
+    }
+
+    return <ProfilePostsView posts={postsToShow} />;
 };
 
 export default ProfilePostsPresenter;
