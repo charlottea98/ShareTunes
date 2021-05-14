ff26639f762f0f7ef36f8cfc77d2989b47ee6056
Johan Lam <johanlam@kth.se>
2021-05-14 17:28:55 +0200

    feat: display animation when a view is loading

--- sharetunes-app/src/components/pages/ProfilePage/ProfileNoData.tsx
+++ sharetunes-app/src/components/pages/ProfilePage/ProfileNoData.tsx
@@ -1,6 +1,13 @@
+import { ProgressLoader } from '../../common/ProgressLoader/ProgressLoader';
+import classes from '../../pages/DiscoverPage/discoverPage.module.scss';
+
 const profileNoData = (userExists: any, sessionStatus: any) => {
     if (userExists === null && sessionStatus !== null) {
-        return <span>currently loading</span>;
+        return (
+            <div className={classes.loader}>
+                <ProgressLoader></ProgressLoader>
+            </div>
+        );
     }
 };
 
